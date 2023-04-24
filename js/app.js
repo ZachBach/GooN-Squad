080c8872318d210632ac564e7e06c84dfe19c215
Zach Auerbach <auernachzach@gmail.com>
2023-04-23 20:14:28 -0700

    damn you webgl

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-import * as THREE from "../node_modules/three/build/three.module.js"
+import * as THREE from 'three'
 // import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader';
